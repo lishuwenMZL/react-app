2c1560718c3721f83d9673a0a3681046c04d76b5
muzili <2483744673@qq.com>
2020-06-18 14:40:36 +0800

    添加左侧导航栏

--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Button } from 'antd'
+import './index.css';
 
-let element = <h1>hello</h1>
+import { Menu } from 'antd';
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 
-// function element () { 
-//   return <h1>hello</h1>
-// }
+const { SubMenu } = Menu;
 
-ReactDOM.render(element, document.getElementById('app'))
+function handleClick(e) {
+  console.log('click', e);
+}
+
+let element = (
+  <Menu
+    onClick={handleClick}
+    style={{ width: 200 }}
+    className="height-full-screent"
+    mode="vertical"
+    theme="dark">
+    
+    <SubMenu key="sub1"
+      icon={<MailOutlined />}
+      title="Navigation One">
+      <Menu.Item key="1">Option 1</Menu.Item>
+      <Menu.Item key="2">Option 2</Menu.Item>
+      <Menu.Item key="3">Option 3</Menu.Item>
+      <Menu.Item key="4">Option 4</Menu.Item>
+    </SubMenu>
+
+    <SubMenu
+      key="sub2"
+      icon={<AppstoreOutlined />}
+      title="Navigation Two">
+      <Menu.Item key="5">Option 5</Menu.Item>
+      <Menu.Item key="6">Option 6</Menu.Item>
+    </SubMenu>
+
+    <SubMenu
+      key="sub4"
+      icon={<SettingOutlined />}
+      title="Navigation Three">
+      <Menu.Item key="9">Option 9</Menu.Item>
+      <Menu.Item key="10">Option 10</Menu.Item>
+      <Menu.Item key="11">Option 11</Menu.Item>
+      <Menu.Item key="12">Option 12</Menu.Item>
+    </SubMenu>
+
+  </Menu>
+)
+
+ReactDOM.render(element, document.getElementById('app'));
